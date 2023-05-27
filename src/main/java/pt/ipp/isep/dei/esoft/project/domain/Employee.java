93ca7b21227ffd9566e9b6a414c363713fd17d80
bernardo barbosa <1220741@isep.ipp.pt>
2023-05-27 17:56:35 +0100

    Empployee superclass and owner class fulfilled

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Employee.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Employee.java
@@ -3,14 +3,27 @@ package pt.ipp.isep.dei.esoft.project.domain;
 import java.util.Objects;
 
 public class Employee {
-    private final String email;
+    private static int employeeIdcounter = 0;
+    private int employeeId;
     private String name;
-    private String position;
-    private String phone;
+    private int ccNumber;
+    private int taxNumber;
+    private final String employeeEmail;
+    private int phoneNumber;
 
-    public Employee(String email) {
-        this.email = email;
+    public Employee( String name, int ccNumber, int taxNumber, String employeeEmail, int phoneNumber) {
+        this.employeeId = employeeIdcounter++;
+        this.name = name;
+        this.ccNumber = ccNumber;
+        this.taxNumber = taxNumber;
+        this.employeeEmail = employeeEmail;
+        this.phoneNumber = phoneNumber;
     }
+
+    public Employee(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -21,25 +34,66 @@ public class Employee {
             return false;
         }
         Employee employee = (Employee) o;
-        return email.equals(employee.email);
+        return employeeEmail.equals(employee.employeeEmail);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(email);
+        return Objects.hash(employeeEmail);
     }
 
     public boolean hasEmail(String email) {
-        return this.email.equals(email);
+        return this.employeeEmail.equals(email);
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    /**
+    public void setCcNumber(int ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public void setTaxNumber(int taxNumber) {
+        this.taxNumber = taxNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static int getEmployeeIdcounter() {
+        return employeeIdcounter;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCcNumber() {
+        return ccNumber;
+    }
+
+    public int getTaxNumber() {
+        return taxNumber;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+/**
      * Clone method.
      *
      * @return A clone of the current instance.
      */
-    public Employee clone() {
-        return new Employee(this.email);
-    }
+
+
 }
--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Owner.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Owner.java
@@ -6,57 +6,21 @@ import pt.isep.lei.esoft.auth.domain.model.Email;
  * The type Owner.
  */
 public class Owner {
-    private static int ownerIdCounter =0;
+    private static int ownerIdCounter = 0;
     private int ownerId;
     private String name;
     private int ccNumber;
-    private String taxNumber;
+    private int taxNumber;
     private final Email ownerEmail;
-    private String phoneNumber;
+    private int phoneNumber;
 
-    public Owner(String name, int ccNumber, String taxNumber, Email emailAddress, String phoneNumber) {
+    public Owner(String name, int ccNumber, int taxNumber, Email ownerEmail, int phoneNumber) {
         this.name = name;
         this.ccNumber = ccNumber;
         this.taxNumber = taxNumber;
-        this.ownerEmail = emailAddress;
+        this.ownerEmail = ownerEmail;
         this.phoneNumber = phoneNumber;
         this.ownerId = ownerIdCounter++;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCcNumber(int ccNumber) {
-        this.ccNumber = ccNumber;
-    }
-
-    public void setTaxNumber(String taxNumber) {
-        this.taxNumber = taxNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getCcNumber() {
-        return ccNumber;
-    }
-
-    public String getTaxNumber() {
-        return taxNumber;
-    }
-
-    public Email getOwnerEmail() {
-        return ownerEmail;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
     }
 
     public static int getOwnerIdCounter() {
@@ -67,8 +31,39 @@ public class Owner {
         return ownerId;
     }
 
-    public Owner clone(){return new Owner(this.name, this.ccNumber, this.taxNumber, this.ownerEmail, this.phoneNumber);
-}
+    public String getName() {
+        return name;
+    }
 
+    public int getCcNumber() {
+        return ccNumber;
+    }
 
+    public int getTaxNumber() {
+        return taxNumber;
+    }
+
+    public Email getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCcNumber(int ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public void setTaxNumber(int taxNumber) {
+        this.taxNumber = taxNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
