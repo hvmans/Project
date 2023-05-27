9e33327e830255731848af0db9f0f363e983e853
bernardo barbosa <1220741@isep.ipp.pt>
2023-05-27 17:06:29 +0100

    Owner and Land class fullfilled

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Land.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Land.java
@@ -9,6 +9,95 @@ public class Land extends Property{
     private String photos;
     private Owner owner;
 
+    public Land(String propertyType, String location, double area, double distanceCenter, String photos, Owner owner, int id, String propertyType1, String location1, double area1, double distanceCenter1, String photos1, Owner owner1) {
+        super(propertyType, location, area, distanceCenter, photos, owner);
+        this.id = id;
+        this.propertyType = propertyType1;
+        this.location = location1;
+        this.area = area1;
+        this.distanceCenter = distanceCenter1;
+        this.photos = photos1;
+        this.owner = owner1;
+    }
+
+    public Land(int id, String propertyType, String location, double area, double distanceCenter, String photos, Owner owner) {
+        this.id = id;
+        this.propertyType = propertyType;
+        this.location = location;
+        this.area = area;
+        this.distanceCenter = distanceCenter;
+        this.photos = photos;
+        this.owner = owner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getPropertyType() {
+        return propertyType;
+    }
+
+    @Override
+    public void setPropertyType(String propertyType) {
+        this.propertyType = propertyType;
+    }
+
+    @Override
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public double getArea() {
+        return area;
+    }
+
+    @Override
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    @Override
+    public double getDistanceCenter() {
+        return distanceCenter;
+    }
+
+    @Override
+    public void setDistanceCenter(double distanceCenter) {
+        this.distanceCenter = distanceCenter;
+    }
+
+    @Override
+    public String getPhotos() {
+        return photos;
+    }
+
+    @Override
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+
+    @Override
+    public Owner getOwner() {
+        return owner;
+    }
+
+    @Override
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
     public Land(String propertyType, String location, double area, double distanceCenter, String photos, Owner owner) {
         this.propertyType = propertyType;
         this.location = location;
@@ -16,6 +105,7 @@ public class Land extends Property{
         this.distanceCenter = distanceCenter;
         this.photos = photos;
         this.owner = owner;
+
     }
 
 }
--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Owner.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Owner.java
@@ -1,4 +1,74 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
+import java.util.Objects;
+
+/**
+ * The type Owner.
+ */
 public class Owner {
+    private static int ownerIdCounter =0;
+    private int ownerId;
+    private String name;
+    private int ccNumber;
+    private String taxNumber;
+    private final String emailAddress;
+    private String phoneNumber;
+
+    public Owner(String name, int ccNumber, String taxNumber, String emailAddress, String phoneNumber) {
+        this.name = name;
+        this.ccNumber = ccNumber;
+        this.taxNumber = taxNumber;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.ownerId = ownerIdCounter++;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCcNumber(int ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    public void setTaxNumber(String taxNumber) {
+        this.taxNumber = taxNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCcNumber() {
+        return ccNumber;
+    }
+
+    public String getTaxNumber() {
+        return taxNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static int getOwnerIdCounter() {
+        return ownerIdCounter;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public Owner clone(){return new Owner(this.name, this.ccNumber, this.taxNumber, this.emailAddress, this.phoneNumber);
+}
+
+
 }
